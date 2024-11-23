f0dc76def3649e3c8fd3cf68f9e5cdd16bf0353d
berkayoflaz94 <berkayoflaz@berkay-MacBook-Pro.local>
2024-11-23 09:59:07 +0300

    auth js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,21 @@
+const userService = require('../services/user')
 const authController = {
     login: async(req,res)=>{
         try{
-            console.log(req.body.email,'email')
-            console.log(req.body.password,'password')
-            res.status(200).send({response:req.body})
+            //const response = await userService.createUser(req.body);
+            res.status(200).send({response:{}})
         }catch(e){
             console.log(e,'error')
         }
         
     },
     register:async(req,res) => {
-        console.log('geldi');
-        res.status(200).send({response:'success'})
+        try{
+            const response = await userService.createUser(req.body);
+            res.status(200).send({response:response})
+        }catch(e){
+            console.log(e,'error')
+        }
     }
 }
 module.exports = authController
